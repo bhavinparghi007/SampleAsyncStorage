6b6a3a5ece8b3116d08124730ac24b003d278b85
Bhavin Parghi <bhavin.parghi007@gmail.com>
2019-06-29 23:44:21 +0530

    add internet check

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Varam from './Varam';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Varam);
